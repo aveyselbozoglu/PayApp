ed52ff8c12ae83ce8aaed12fc25054a9c6245b95
KJavelin <kjavelin@hotmail.com>
2020-04-26 01:12:01 +0300

    Added rotativa pdfchanger..some bug at pagination

--- Payer.Solution/Payer/Controllers/PayController.cs
+++ Payer.Solution/Payer/Controllers/PayController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Payer.BusinessLayer;
 using Payer.Entities;
+using Rotativa;
 
 
 namespace Payer.Controllers
@@ -17,10 +18,17 @@ namespace Payer.Controllers
 
         private BusinessLayerResult<PaymentRecord> blResultPaymentRecord = new BusinessLayerResult<PaymentRecord>();
         // GET: Pay
-        public ActionResult Index()
+        public ActionResult Index(int? pageNumber)
         {
             blResultPaymentRecord =payComputationManager.GetAllPayments();
-            return View(blResultPaymentRecord.BlResultList);
+            ListPagination<PaymentRecord> p = null;
+
+            if (blResultPaymentRecord.BlResultList != null)
+            {
+                p = ListPagination<PaymentRecord>.Create(blResultPaymentRecord.BlResultList, pageNumber ?? 1, 3);
+            }
+
+            return View(p);
         }
 
         public ActionResult Create()
@@ -61,6 +69,42 @@ namespace Payer.Controllers
 
         }
 
+        public ActionResult Payslip(int? id)
+        {
+            if (id != null)
+            {
+                blResultPaymentRecord = payComputationManager.GetPaymentRecordById(id);
+                return View(blResultPaymentRecord.BlResult);
+            }
+            return RedirectToAction("Index");
+        }
 
+        public ActionResult GeneratePayslipPdf(int? id)
+        {
+            if (id != null)
+            {
+                blResultPaymentRecord = payComputationManager.GetPaymentRecordById(id);
+                //var payslip =
+                //return new ViewAsPdf("Payslip", new {id = id})
+                //{
+                //    FileName = blResultPaymentRecord.BlResult.EmployeeId+"_"+"_"+DateTime.Now+"_payslip.pdf"
+                //};
+                // return payslip;
+                if (blResultPaymentRecord.BlResult != null)
+                {
+
+                    return new ViewAsPdf("GeneratePayslipPdf", blResultPaymentRecord.BlResult)
+                    {
+                        FileName = blResultPaymentRecord.BlResult.EmployeeId + "_" + DateTime.Now.ToLocalTime() + "_payslip.pdf",
+                        
+                        PageSize = Rotativa.Options.Size.A4,
+
+
+                    };
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Payer.Solution/Payer/WebClasses/ListPagination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Payer
+{
+    public class ListPagination<T> : List<T>
+    {
+
+        public int PageIndex { get;private set; }
+        public int TotalPages { get;private set; }
+
+        public bool IsPreviousPageAvailable() => PageIndex > 1;
+
+        public bool IsNextPageAvailable() => PageIndex < TotalPages;
+
+
+        public static ListPagination<T> Create(List<T> source,int pageIndex, int pageSize)
+        {
+            var count = source.Count;
+            var items = source.Skip((pageIndex - 1 )* pageSize).Take(pageSize).ToList();
+            return new ListPagination<T>(items,count,pageIndex,pageSize);
+
+        }
+
+        public ListPagination(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+    }
+}
